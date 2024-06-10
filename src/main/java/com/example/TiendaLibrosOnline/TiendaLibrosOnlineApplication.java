1aceef395504726a658a86e4d0f11b89f7ab3d31
AbelBusi <cesarabelbustamante@gmail.com>
2024-06-10 11:43:47 -0500

    Creando el repositorio local del proyecto

---
+++ src/main/java/com/example/TiendaLibrosOnline/TiendaLibrosOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.TiendaLibrosOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLibrosOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLibrosOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TiendaLibrosOnline/TiendaLibrosOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TiendaLibrosOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibrosOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
