ae6841eb2a0c200cc8ff5b55a319c05fd1e6c5bd
AbelBusi <cesarabelbustamante@gmail.com>
2024-06-10 18:38:54 -0500

    Guardando la primera plantilla html

---
+++ src/main/java/com/example/TiendaLibrosOnline/controller/HomeBooksController.java
@@ -0,0 +1,18 @@
+package com.example.TiendaLibrosOnline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeBooksController {
+
+    @GetMapping("")
+    public String mostrar(){
+
+        return "hola.html";
+
+    }
+    
+}
