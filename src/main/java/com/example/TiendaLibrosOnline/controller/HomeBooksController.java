24bf653cf085a64bf72113a34e3ca45e1f3edd4b
AbelBusi <cesarabelbustamante@gmail.com>
2024-06-24 16:55:00 -0500

    Se implemento los primeros input con su respectivo objteo

--- src/main/java/com/example/TiendaLibrosOnline/controller/HomeBooksController.java
+++ src/main/java/com/example/TiendaLibrosOnline/controller/HomeBooksController.java
@@ -15,5 +15,9 @@ public class HomeBooksController {
         return "home/homeBook";
 
     }
+    
+    
+    
+    
 
 }
--- src/main/java/com/example/TiendaLibrosOnline/controller/UsuarioController.java
+++ src/main/java/com/example/TiendaLibrosOnline/controller/UsuarioController.java
@@ -1,12 +1,23 @@
 package com.example.TiendaLibrosOnline.controller;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.TiendaLibrosOnline.model.Usuario;
+
+
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+	
+	private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
 
     @GetMapping("/ingresar")
     public String iniciarSesion(){
@@ -16,8 +27,21 @@ public class UsuarioController {
     }
 
     @GetMapping("/crearCuenta")
-    public String registrarUsuario(){
+    public String registrarUsuario(Model model){
+    	
+    	Usuario usuario= new Usuario();
+    	
+    	model.addAttribute("usuarioForm",usuario );
+    	
         return "acceso/registerUser";
+    }
+    
+    @PostMapping("/guardarUsuario")
+    public String guardarUsuario(@ModelAttribute Usuario usuario,Model model ) {
+    	
+    	model.addAttribute("usuarioForm", new Usuario());
+    	logger.info("Usuario {}",usuario);
+    	return "redirect:/home";
     }
 
 
