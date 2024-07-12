12281b780eb146d0bbdafefcc8e6010234f1f86a
AbelBusi <cesarabelbustamante@gmail.com>
2024-07-12 17:49:35 -0500

    Guardando plantilla html de admin

--- src/main/java/com/example/TiendaLibrosOnline/controller/AdminController.java
+++ src/main/java/com/example/TiendaLibrosOnline/controller/AdminController.java
@@ -4,22 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@GetMapping("/homeConfigurer")
 	public String home() {
 		return "administrador/homeAdmin";
 	}
 
 	@GetMapping("/agregarProducto")
-	public String producto(){
+	public String producto() {
 
 		return "administrador/addLibro";
 
 	}
-	
-	
+
 }
