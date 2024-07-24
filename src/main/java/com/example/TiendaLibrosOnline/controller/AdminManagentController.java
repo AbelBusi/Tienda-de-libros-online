006ea526db6045fba7a6cebc762e5b3bc12a13fd
AbelBusi <cesarabelbustamante@gmail.com>
2024-07-23 23:57:13 -0500

    se establevio las direcciones url correctamente para cada plantilla

--- src/main/java/com/example/TiendaLibrosOnline/controller/AdminManagentController.java
+++ src/main/java/com/example/TiendaLibrosOnline/controller/AdminManagentController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("managent")
+@RequestMapping("/admin/managent")
 public class AdminManagentController {
 	
 	@GetMapping("/categories")
